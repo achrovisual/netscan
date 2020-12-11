912649ef11dcf34fc458fc8d542c592bc411c068
Eugenio Pastoral <stonenobrein@gmail.com>
2020-12-11 23:29:40 +0800

    Separate modules

--- netscan.py
+++ netscan.py
@@ -1,67 +1,8 @@
 #!/usr/bin/env python3
 
-import socket, re, argparse, time
-from scapy.all import *
+import argparse, time, port_scan
 
 src_ip = get_if_addr(conf.iface)
-
-def is_up(ip):
-    icmp = IP(dst=ip)/ICMP()
-    resp = sr1(icmp, timeout=10, verbose = 0)
-    if resp == None:
-        return False
-    else:
-        return True
-
-def probe_port(ip, port, flags, flag, scan_type, result = 1):
-    src_port = RandShort()
-    try:
-        p = IP(dst=ip)/TCP(sport=src_port, dport=port, flags=flags)
-
-        if flags == 'S':
-            print(src_ip + ' > SYN > ' + ip)
-        elif flags == 'FPU':
-            print(src_ip + ' > FIN, PSH, URG > ' + ip)
-        elif flags == 'F':
-            print(src_ip + ' > FIN > ' + ip)
-        elif flags == 'A':
-            print(src_ip + ' > ACK > ' + ip)
-        elif flags == '':
-            print(src_ip + ' > NULL > ' + ip)
-
-        resp = sr1(p, timeout = 2, verbose = 0) # Send packet
-        rcv_ip = resp.getlayer(IP).src
-
-        if str(type(resp)) == "<class 'NoneType'>":
-            result = 1
-        elif resp.haslayer(TCP):
-            if resp.getlayer(TCP).flags == 0x12:
-                print(src_ip + ' < SYN, ACK < ' + ip)
-                if scan_type == 2:
-                    send_rst = sr(IP(dst=ip)/TCP(sport=src_port, dport=port, flags='R'), timeout = 2, verbose = 0)
-                    print(src_ip + ' > RST > ' + ip)
-                elif scan_type == 1:
-                    send_ack = send(IP(dst=ip)/TCP(sport=src_port, dport=port, flags='A', ack = resp.seq + 1, seq = resp.ack + 1), verbose = 0)
-                    print(src_ip + ' > ACK > ' + ip)
-                result = 1
-            elif resp.getlayer(TCP).flags == flag:
-                if flag == 0x14:
-                    print(src_ip + ' < RST, ACK < ' + ip)
-                elif flag == 0x4:
-                    print(src_ip + ' < RST < ' + ip)
-                result = 0
-            elif (int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]):
-                result = 2
-    except AttributeError as e:
-        print('No response')
-        pass
-    except ValueError as e:
-        print('No response')
-        result = 0
-    except Exception as e:
-        print(e)
-        pass
-    return result
 
 def main():
     start = time.time()
@@ -105,7 +46,7 @@ def main():
         flag = 0x4
 
     if is_up(ip):
-        response = probe_port(ip, int(port), flags, flag, type)
+        response = port_scan.probe_port(ip, int(port), flags, flag, type)
         if response == 1:
             openp = port
             filterdp = ''
