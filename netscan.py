e09df1fe665a6168df24de43e089979bcb8b3469
eugeniopastoral <stonenobrein@gmail.com>
2020-12-12 00:58:30 +0800

    Added support for Ping Sweep

--- netscan.py
+++ netscan.py
@@ -1,84 +1,72 @@
 #!/usr/bin/env python3
 
-import argparse, time, port_scan
+import argparse, time, scanner, ipv4
 
 def main():
     start = time.time()
+
     # Arg Parsing
-    parser = argparse.ArgumentParser(description='', formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description = '', formatter_class = argparse.RawTextHelpFormatter)
+
     parser.add_argument("H",  help = "Target Host IP or URL")
-    parser.add_argument("-p", "--p", help = "Targeted Port(s), comma delimited", required = "True")
-    parser.add_argument("-t", "--t", type = int, help = "Port Scan Type. The following port scan types can be used:\n1) TCP Connect Scan\n2) TCP SYN Scan\n3) XMAS Scan\n4) FIN Scan\n5) Null Scan\n6) TCP ACK Scan", required = "True")
+    parser.add_argument("-p", "--p", help = "Target Port")
+    parser.add_argument("-i", "--i", action = 'store_true', help = "Perform Ping Sweep")
+    parser.add_argument("-c", "--c", help = "Number of ICMP ECHO Requests to be sent for Ping Sweep", type = int)
+    parser.add_argument("-t", "--t", action = 'store_true', help = "Perform TCP Connect Scan")
+    parser.add_argument("-s", "--s", action = 'store_true', help = "Perform TCP SYN Scan")
+    parser.add_argument("-x", "--x", action = 'store_true', help = "Perform TCP XMAS Scan")
+    parser.add_argument("-f", "--f", action = 'store_true', help = "Perform TCP FIN Scan")
+    parser.add_argument("-n", "--n", action = 'store_true', help = "Perform TCP NULL Scan")
+    parser.add_argument("-a", "--a", action = 'store_true', help = "Perform TCP ACK Scan")
+    parser.add_argument("-ALL", "--ALL", action = 'store_true', help = "Perform ALL TCP Port Scans")
+    parser.add_argument("-T", "--T", action = 'store_true', help = "Show time spent to compelete the scan")
+
     args = parser.parse_args()
     ip = args.H
-    type = args.t
+    end = 2
+
+    if args.i:
+        end = 1
+    elif args.t:
+        type = 1
+    elif args.s:
+        type = 2
+    elif args.x:
+        type = 3
+    elif args.f:
+        type = 4
+    elif args.n:
+        type = 5
+    elif args.a:
+        type = 6
+    elif args.ALL:
+        end = 7
     port = args.p
-    # Check args present and set variables
-    if (ip == None) | (port == None):
+
+    if not ipv4.validate_ip(ip):
+        print('\nIP address is invalid. Please try again.')
         parser.print_help()
         exit(0)
 
-    if type == 1:
-        print('\nPerforming TCP Connect Scan on ' + ip + ':' + port + '.\n')
-        flags = 'S'
-        flag = '0x14'
-    elif type == 2:
-        print('\nPerforming TCP SYN Scan on ' + ip + ':' + port + '.\n')
-        flags = 'S'
-        flag = 0x14
-    elif type == 3:
-        print('\nPerforming TCP XMAS Scan on ' + ip + ':' + port + '.\n')
-        flags = 'FPU'
-        flag = 0x14
-    elif type == 4:
-        print('\nPerforming TCP FIN Scan on ' + ip + ':' + port + '.\n')
-        flags = 'F'
-        flag = 0x14
-    elif type == 5:
-        print('\nPerforming TCP NULL Scan on ' + ip + ':' + port + '.\n')
-        flags = ''
-        flag = 0x14
-    elif type == 6:
-        print('\nPerforming TCP ACK Scan on ' + ip + ':' + port + '.\n')
-        flags = 'A'
-        flag = 0x4
+    # Check args present and set variables
+    if not args.i:
+        if (port == None):
+            print('\nPlease specify a valid port to perform a port scan.')
+            parser.print_help()
+            exit(0)
 
-    if port_scan.is_up(ip):
-        response = port_scan.probe_port(ip, int(port), flags, flag, type)
-        if response == 1:
-            openp = port
-            filterdp = ''
-        elif response == 2:
-            filterdp = port
-            openp = ''
-        else:
-            filterdp, openp = '', ''
+    if args.i:
+        scanner.ping(ip, args.c)
 
-        if type == 1:
-            if response == 1:
-                print('\n' + port + " is open.")
-            else:
-                print('\n' + port + " is closed.")
-
-        if type == 2 or type == 3 or type == 4 or type == 5:
-            if openp != '':
-                print('\n' + openp + " is possibly open or filtered.")
-            if filterdp != '':
-                print('\n' + filterdp + " is filtered.")
-            if (openp == '') and (filterdp == ''):
-                print('\n' + port + " is closed.")
-        if type == 6:
-            if openp != '':
-                print('\n' + openp + " is filtered by stateful firewall.")
-            if filterdp != '':
-                print('\n' + filterdp + " is filtered by stateful firewall.")
-            if (openp == '') and (filterdp == ''):
-                print('\n' + port + " is not filtered.")
-    else:
-        print("Host is down.")
+    for type in range (1, end):
+        scanner.portscan(ip, port, type)
+        print('\n=============================================')
 
     end = time.time()
 
-    print('\nCompleted scan in ' + str(round((end - start), 4)) + 's.')
+    if args.T:
+        print('\nCompleted scan in ' + str(round((end - start), 4)) + 's.')
+    else:
+        print('\nScan complete.')
 if __name__ == '__main__':
     main()
---
+++ scanner.py
@@ -0,0 +1,70 @@
+import port_scan
+
+def portscan(ip, port, type):
+    flag, flags = parse_type(ip, port, type)
+    if port_scan.is_up(ip):
+        response = port_scan.probe_port(ip, int(port), flags, flag, type)
+        if response == 1:
+            openp = port
+            filterdp = ''
+        elif response == 2:
+            filterdp = port
+            openp = ''
+        else:
+            filterdp, openp = '', ''
+
+        if type == 1:
+            if response == 1:
+                print('\n' + port + " is open.")
+            else:
+                print('\n' + port + " is closed.")
+
+        if type == 2 or type == 3 or type == 4 or type == 5:
+            if openp != '':
+                print('\n' + openp + " is possibly open or filtered.")
+            if filterdp != '':
+                print('\n' + filterdp + " is filtered.")
+            if (openp == '') and (filterdp == ''):
+                print('\n' + port + " is closed.")
+        if type == 6:
+            if openp != '':
+                print('\n' + openp + " is filtered by stateful firewall.")
+            if filterdp != '':
+                print('\n' + filterdp + " is filtered by stateful firewall.")
+            if (openp == '') and (filterdp == ''):
+                print('\n' + port + " is not filtered.")
+    else:
+        print("Host is down.")
+
+
+
+def parse_type(ip, port, type):
+    if type == 1:
+        print('\nPerforming TCP Connect Scan on ' + ip + ':' + port + '.\n')
+        return 0x14, 'S'
+
+    elif type == 2:
+        print('\nPerforming TCP SYN Scan on ' + ip + ':' + port + '.\n')
+        return 0x14, 'S'
+
+    elif type == 3:
+        print('\nPerforming TCP XMAS Scan on ' + ip + ':' + port + '.\n')
+        return 0x14, 'FPU'
+
+    elif type == 4:
+        print('\nPerforming TCP FIN Scan on ' + ip + ':' + port + '.\n')
+        return 0x14, 'F'
+
+    elif type == 5:
+        print('\nPerforming TCP NULL Scan on ' + ip + ':' + port + '.\n')
+        return 0x14, ''
+
+    elif type == 6:
+        print('\nPerforming TCP ACK Scan on ' + ip + ':' + port + '.\n')
+        return 0x4, 'A'
+
+def ping(ip, n):
+    print('\nPerforming Ping Sweep on ' + ip + '.\n')
+    for i in range(n):
+        if port_scan.is_up(ip):
+            print(ip + ' is alive.')
