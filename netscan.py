32186e43b84270508f4ffc4c2a86f3295e3d9adc
eugeniopastoral <stonenobrein@gmail.com>
2020-12-13 11:04:20 +0800

    Improved error handling

--- netscan.py
+++ netscan.py
@@ -20,19 +20,26 @@ def main():
     # Arg Parsing
     parser = argparse.ArgumentParser(description = '', formatter_class = argparse.RawTextHelpFormatter)
 
-    parser.add_argument("H", nargs="?", help = "Target Host IP or URL")
-    parser.add_argument("-p", "--p", help = "Target Port")
-    parser.add_argument("-i", "--i", action = 'store_true', help = "Perform Ping Sweep")
-    parser.add_argument("-c", "--c", help = "Number of ICMP ECHO Requests to be sent for Ping Sweep", type = int)
-    parser.add_argument("-t", "--t", action = 'store_true', help = "Perform TCP Connect Scan")
-    parser.add_argument("-s", "--s", action = 'store_true', help = "Perform TCP SYN Scan")
-    parser.add_argument("-x", "--x", action = 'store_true', help = "Perform TCP XMAS Scan")
-    parser.add_argument("-f", "--f", action = 'store_true', help = "Perform TCP FIN Scan")
-    parser.add_argument("-n", "--n", action = 'store_true', help = "Perform TCP NULL Scan")
-    parser.add_argument("-a", "--a", action = 'store_true', help = "Perform TCP ACK Scan")
-    parser.add_argument("-ALL", "--ALL", action = 'store_true', help = "Perform ALL TCP Port Scans")
-    parser.add_argument("-T", "--T", action = 'store_true', help = "Show time spent to compelete the scan")
-    parser.add_argument("-v", "--v", action = 'store_true', help = "Show program description")
+    THO = parser.add_argument_group('TARGET HOST OPTIONS')
+    THO.add_argument("H", nargs="?", help = "Target Host IP or URL")
+    THO.add_argument("-p", "--p", help = "Target Port")
+
+    PSO = parser.add_argument_group('PORT SCANNING OPTIONS')
+    PSO.add_argument("-t", "--t", action = 'store_true', help = "Perform TCP Connect Scan")
+    PSO.add_argument("-s", "--s", action = 'store_true', help = "Perform TCP SYN Scan")
+    PSO.add_argument("-x", "--x", action = 'store_true', help = "Perform TCP XMAS Scan")
+    PSO.add_argument("-f", "--f", action = 'store_true', help = "Perform TCP FIN Scan")
+    PSO.add_argument("-n", "--n", action = 'store_true', help = "Perform TCP NULL Scan")
+    PSO.add_argument("-a", "--a", action = 'store_true', help = "Perform TCP ACK Scan")
+    PSO.add_argument("-ALL", "--ALL", action = 'store_true', help = "Perform ALL TCP Port Scans")
+
+    IEO = parser.add_argument_group('PING SWEEPING OPTIONS')
+    IEO.add_argument("-i", "--i", action = 'store_true', help = "Perform Ping Sweep")
+    IEO.add_argument("-c", "--c", help = "Number of ICMP ECHO Requests to be sent for Ping Sweep", type = int)
+
+    IEO = parser.add_argument_group('PROGRAM OPTIONS')
+    IEO.add_argument("-T", "--T", action = 'store_true', help = "Show time spent to compelete the scan")
+    IEO.add_argument("-v", "--v", action = 'store_true', help = "Show program description")
 
     args = parser.parse_args()
 
@@ -50,27 +57,48 @@ def main():
         print("NOTE: In order to ensure that the program is going to work correctly, please install the latest version of Scapy.")
     else:
         ip = args.H
-
-        if args.i:
-            end = 1
-        elif args.t:
-            type = 1
-        elif args.s:
-            type = 2
-        elif args.x:
-            type = 3
-        elif args.f:
-            type = 4
-        elif args.n:
-            type = 5
-        elif args.a:
-            type = 6
-        elif args.ALL:
-            end = 7
+        type = None
+        try:
+            if args.t:
+                if type != None:
+                    raise Exception()
+                type = 1
+            if args.s:
+                if type != None:
+                    raise Exception()
+                type = 2
+            if args.x:
+                if type != None:
+                    raise Exception()
+                type = 3
+            if args.f:
+                if type != None:
+                    raise Exception()
+                type = 4
+            if args.n:
+                if type != None:
+                    raise Exception()
+                type = 5
+            if args.a:
+                if type != None:
+                    raise Exception()
+                type = 6
+            if args.ALL:
+                if type != None:
+                    raise Exception()
+                type = 0
+                end = 7
+            if args.i or args.c != None:
+                if type != None or end == 7:
+                    raise Exception()
+        except Exception as e:
+            print('\nInvalid argument combination supplied. Try again.')
+            parser.print_help()
+            exit(0)
         port = args.p
 
-        if not ipv4.validate_ip(ip):
-            print('\nIP address is missing or invalid. Please try again.')
+        if type == None or ip == None:
+            print('\nInsufficient arguments supplied. Try again.')
             parser.print_help()
             exit(0)
 
@@ -80,6 +108,11 @@ def main():
                 print('\nPlease specify a valid port to perform a port scan.')
                 parser.print_help()
                 exit(0)
+
+        if not ipv4.validate_ip(ip):
+            print('\nIP address is missing or invalid. Please try again.')
+            parser.print_help()
+            exit(0)
 
         if args.i:
             if args.c == None:
